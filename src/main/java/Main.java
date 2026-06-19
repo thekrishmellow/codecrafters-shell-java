607197fd28009e1e806ee8266fb1ace5ba396a87
thekrishmellow <krishnabhatia1908@gamil.com>
2026-06-19 11:35:19 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,24 +4,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         // System.out.print("$ ");
-        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("$ ");
-            if (!scanner.hasNextLine()) {
-                break;
-            }
-            String input = scanner.nextLine();
+        String input = scanner.nextLine();
             if (input.equals("exit")) {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
-            } else if (input.equals("echo")) {
-                System.out.println();
             } else {
                 System.out.println(input + ": command not found");
             }
-        }
     }
 }
