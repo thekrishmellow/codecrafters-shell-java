3feb4a08b0cd65edb8e07448c3712a5eb78f1ff0
thekrishmellow <krishnabhatia1908@gamil.com>
2026-06-19 11:37:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            } else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
-            } else {
-                System.out.println(input + ": command not found");
-            }
+        if (input.equals("exit")) {
+            return;
+        } else if (input.startsWith("echo ")) {
+            System.out.println(input.substring(5));
+        } else {
+            System.out.println(input + ": command not found");
+        }
     }
 }
