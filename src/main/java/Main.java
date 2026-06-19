d1aea00055ab3dac8bebea18806ecba72901ff35
thekrishmellow <krishnabhatia1908@gamil.com>
2026-06-19 11:14:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         // System.out.print("$ ");
+        System.out.print("$ ");
     }
 }
